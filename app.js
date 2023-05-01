8827104aa96201accf54528052d1981ede91a3cf
Martin Hunt <martinseanhunt@gmail.com>
2023-05-02 00:20:30 +0100

    tiny tweak to comments

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   appToken: process.env.SLACK_APP_TOKEN,
-  socketMode: true,
+  socketMode: true, // Connect using websockets, no need for ngrok or similar.
 });
 
 app.event('message', async ({ event, client }) => {
@@ -75,7 +75,7 @@ app.event('message', async ({ event, client }) => {
 });
 
 (async () => {
-  // Start your app
+  // Start the app
   await app.start(process.env.PORT || 3000);
   console.log('⚡️ Bolt app is running!');
 })();
