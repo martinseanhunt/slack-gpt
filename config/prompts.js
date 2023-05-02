fee91054bbb2a3d5719d099e7711a83b785aa454
Martin Hunt <martinseanhunt@gmail.com>
2023-05-02 22:43:51 +0100

    edit prompts and tweak settings for docs bot, remove chat history to save tokens

--- config/prompts.js
+++ config/prompts.js
@@ -1,6 +1,7 @@
-export const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+export const QA_PROMPT = `You are a helpful AI assistant with expertise in JavaScript, Node.js, Next.js, Chat GPT, and TypeScript. Use the following pieces of context from Langchain documentation to answer the question at the end. Provide a detailed answer with code examples where possible.
+
+If you don't know the answer via the context or the question is unrelated to your areas of expertise, just say you don't know. DO NOT try to make up an answer.
+
 Answer in formatted mrkdwn, use only Slack-compatible mrkdwn, such as bold (*text*), italic (_text_), strikethrough (~text~), and lists (1., 2., 3.).
 
 =========
